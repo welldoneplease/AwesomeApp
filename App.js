bf8eb81906782669ed03ca68bdfa0976e034d163
welldoneplease <welldoneplease@byom.de>
2018-08-07 13:29:17 +0200

    Test

--- App.js
+++ App.js
@@ -7,14 +7,9 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const instructions = 'Double tap R on your keyboard to reload,\n Shake or press menu button for dev menu';
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -43,7 +38,7 @@ const styles = StyleSheet.create({
   },
   instructions: {
     textAlign: 'center',
-    color: 'green',
+    color: 'yellow',
     marginBottom: 5,
   },
 });
---
+++ __tests__/App.test.js
@@ -0,0 +1,14 @@
+import 'react-native';
+import React from 'react';
+
+import renderer from 'react-test-renderer';
+
+import App from '../App';
+
+describe('App', () => {
+  test('renders correctly', () => {
+    const tree = renderer.create(<App />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
